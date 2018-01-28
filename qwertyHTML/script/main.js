a2ee8b4b799bbe4985787251ec3401ce02605e73
VoitenkoSergei <sv.voitenko@gmail.com>
2018-01-29 00:05:20 +0200

    modified some details in the style of media queries

--- qwertyHTML/script/main.js
+++ qwertyHTML/script/main.js
@@ -1,8 +1,6 @@
 (function(){
 	var log = document.getElementById('upload');
 	var logSub = document.getElementById('login_sub');
-	console.log(log); 
-	console.log(logSub);
 	log.onclick =function(){
 		logSub.classList.toggle('sub');
 	}
