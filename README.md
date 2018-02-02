909b80cf2878cb1a9298d2e4e49aa81838e47a93
VoitenkoSergei <sv.voitenko@gmail.com>
2018-02-03 00:48:24 +0200

    added SideBar

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # testQwerty
 test tasks for qwerty.software
+01.The remark on the first work is fulfilled.
+
--- qwertyHTML/script/main.js
+++ qwertyHTML/script/main.js
@@ -1,7 +1,14 @@
 (function(){
 	var log = document.getElementById('upload');
 	var logSub = document.getElementById('login_sub');
+	var button = document.getElementById('sidebarCollapse');
+	var sidebar =document.getElementById('sidebar');
+	
 	log.onclick =function(){
 		logSub.classList.toggle('sub');
+	}
+
+	button.onclick = function(){
+		sidebar.classList.toggle('active');
 	}
 })();
